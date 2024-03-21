09d802ddd68d7abd95a4a735fb8a4aa5707930d9
mazedabegum <mazedabegum523@gmail.com>
2024-03-21 19:26:11 +0600

    first commit

---
+++ form.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+const FirstName = document.querySelector("FirstName");
+const LastName = document.querySelector("LastName");
+console.log("LastName");
+
+
+
+
+
+
+// form.addEventListener("submit", formhandler);
+// function formhandler(e){
+//     e.preventDefault();
+//     console.log("submit");
+// }
+
+
+function formhandler(e){
+const forminfo = {
+     FirstName: FirstName.value,
+    LastName: LastName.value,
+    Email: Email.value,
+    password: password,
+
+}
+
+
+console.log("forminfo");
+}
+
+
